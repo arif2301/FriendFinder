98362773ba20d663bfdf3b0a26c98ea4168066e7
neil <arifrahman@hotmail.com>
2019-09-06 18:19:19 -0400

    server and html started

---
+++ server.js
@@ -0,0 +1,26 @@
+// npm packages
+var express = require("express");
+var path = require("path");
+
+// creating express server
+var app = express();
+
+// Sets an initial port
+var PORT = process.env.PORT || 8080;
+
+// Sets up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// ROUTER
+// "route" files.
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// LISTENER
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
