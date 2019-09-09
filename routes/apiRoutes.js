c65bb90d25dd04c3267ee43ab4aaec161c041549
neil <arifrahman@hotmail.com>
2019-09-08 20:26:29 -0400

    logic to determine match is completed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,11 +15,30 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
-  
+
   app.post("/api/friends", function(req, res) {
   // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
   // It will do this by sending out the value "true" have a table
   // req.body is available since we're using the body parsing middleware
     friends.push(req.body);
+    var userid = friends.length;
+    var match;
+    var lowScore = 40;
+    for (x=0; x< (userid-1); x++) {
+
+      var total = 0;
+      for (y=0; y <10; y++){
+        var difference = Math.abs(friends[userid].scores[y] - friends[x].scores(y));
+        total += difference;
+      }
+      if (total < lowScore){
+        lowScore = total;
+        match = friends[x]
+      }
+
+    }
+
+
+    res.json(match)
   });
 }
