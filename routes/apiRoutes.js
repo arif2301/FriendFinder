3969599792f5ec1afe6fb396e84a60232754039d
neil <arifrahman@hotmail.com>
2019-09-13 18:35:06 -0400

    trying heroku deployment

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,7 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
 
-var friends = require("../data/friends");
+// LOAD DATA
+
+var friends = require("../data/friends.js");
 
 
 // ===============================================================================
@@ -17,28 +14,30 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-  // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-  // It will do this by sending out the value "true" have a table
   // req.body is available since we're using the body parsing middleware
     friends.push(req.body);
-    var userid = friends.length;
+    var userid = friends.length -1;
+    console.log(userid)
+    console.log("userid : "+ userid);
     var match;
     var lowScore = 40;
     for (x=0; x< (userid-1); x++) {
 
       var total = 0;
       for (y=0; y <10; y++){
-        var difference = Math.abs(friends[userid].scores[y] - friends[x].scores(y));
+        var difference = Math.abs(parseInt(friends[userid].scores[y]) - parseInt(friends[x].scores[y]));
         total += difference;
       }
       if (total < lowScore){
         lowScore = total;
         match = friends[x]
       }
+      console.log("match score: " + lowScore + " for " + match.name);
 
     }
 
-
-    res.json(match)
+    console.log (match);
+    res.json(friends[0])
+    //res.json(match)
   });
 }
